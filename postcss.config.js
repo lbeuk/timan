631ee7dc4207ca9ec43115bfe1d0ee595e52f147
Luke Beukelman <lbeuk@protonmail.com>
2023-11-02 17:41:19 -0600

    Syncing changes

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+import tailwind from 'tailwindcss'
+import tailwindConfig from './tailwind.config.js'
+export default {
+    plugins: [tailwind(tailwindConfig)]
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script>
+	import '../app.postcss';
+	import { HomeIcon, CalendarIcon, SettingsIcon, XIcon } from 'svelte-feather-icons';
+	import { appWindow } from '@tauri-apps/api/window';
+
+</script>
+
+<div data-tauri-drag-region class="titlebar flex flex-row text-zinc-400 bg-zinc-800">
+	<div on:click={appWindow.close()} on:keydown={appWindow.close()} role="button" tabindex="0">
+		<XIcon />
+	</div>
+</div>
+<div class="flex flex-row">
+	<div
+		class="flex flex-col text-c h-screen p-2 space-y-2 border-r border-zinc-500 text-zinc-400 bg-zinc-800"
+	>
+		<a href="/"><HomeIcon /></a>
+		<a href="/calendar"><CalendarIcon /></a>
+		<div class="grow" />
+		<a href=""><SettingsIcon /></a>
+	</div>
+	<div class="p-2">
+		<slot />
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.zinc.900);
+		color: theme(colors.zinc.50);
+	}
+</style>
